238eaf22e4ccbdb93805b42af32fbfc154ed7889
violet978 <villette@live.com>
2022-05-31 22:12:56 -0700

    Added jumbotron present time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
-// Universal variables first
+// Jumbotron present day,date that displays under header
+var today = moment();
+// Same as getelementbyid in jquery
+$("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
 
