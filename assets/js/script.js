6ece6d4addb2c4417a4d92bc9b2648913dbe8826
violet978 <villette@live.com>
2022-05-31 22:13:40 -0700

    Added function for adding each row div

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,29 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
 
+// times array that will display
+var hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+//Creates HTML elements for our planner's time rows
+hoursArray.forEach(function(hour) {
+    // Variables for each 'create element'
+    var rowDiv = $('<div>');
+    var timeColumn = $('<div>');
+    var textColumn = $('<textarea>');
+    var saveButtonColumn = $('<button>');
+
+    // Add the classes we need to each element
+    rowDiv.addClass('row time-block');
+    timeColumn.addClass('col-1 hour');
+    textColumn.addClass('col-10 description');
+    saveButtonColumn.addClass('col-1 btn saveBtn');
+
+    rowDiv.attr('id', hour);
+    // add text to time
+    timeColumn.text(hour + ':00');
+    // add text to save button
+    saveButtonColumn.text('save');
+
+    // Append the time, text, save button, to each row div, and then to the container
+    rowDiv.append(timeColumn, textColumn, saveButtonColumn);
+    $('.container').append(rowDiv);
+})
